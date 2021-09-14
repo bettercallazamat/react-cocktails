65b5f2dbf9cb0c538783b8f3a82ec4021917fa88
bettercallazamat <bettercallazamat@gmail.com>
2021-09-14 07:36:59 +0600

    Add filter functionality

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,37 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const CategoryFilter = () => (
-  <div className="CategoryFilter">
-    <select />
-  </div>
-);
+const CategoryFilter = ({ filter, cocktails }) => {
+  const options = cocktails.map((cocktail) => (
+    <option value={cocktail.strDrink} key={cocktail.idDrink}>
+      {cocktail.strDrink}
+    </option>
+  ));
+
+  const defaultOption = () => (
+    <option value="All" key="1" selected>
+      All
+    </option>
+  );
+
+  return (
+    <div className="searchWrapper d-flex  justify-content-center">
+      <select className="searchBar text-center " onChange={filter}>
+        {defaultOption()}
+        {options}
+      </select>
+    </div>
+  );
+};
+
+CategoryFilter.propTypes = {
+  filter: PropTypes.func,
+  cocktails: PropTypes.objectOf.isRequired,
+};
+
+CategoryFilter.defaultProps = {
+  filter: () => {
+  },
+};
 
 export default CategoryFilter;
--- src/containers/CocktailsList.js
+++ src/containers/CocktailsList.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import CocktailCard from '../components/CocktailCard';
+import CategoryFilter from '../components/CategoryFilter';
+import filterAction from '../actions/filters';
 import '../assets/styles/CocktailsList.css';
 
-const CocktailsList = ({ cocktails }) => (
-  <div className="CocktailsList">
-    <div className="CocktailsList-filter" />
-    <div className="CocktailsList-list">
-      {
-        cocktails.map((cocktail) => <CocktailCard cocktail={cocktail} key={cocktail} />)
+const CocktailsList = ({ cocktails }) => {
+  const filter = useSelector((state) => state.filter);
+  const dispatch = useDispatch();
+  const filteredCocktails = (filter !== 'All') ? cocktails.filter((cocktail) => cocktail.strDrink === filter) : cocktails;
+
+  const resetFilter = () => {
+    dispatch(filterAction('All'));
+  };
+
+  const handleFilterChange = (e) => {
+    dispatch(filterAction(e.target.value));
+  };
+
+  return (
+    <div className="CocktailsList">
+      <div className="CocktailsList-filter">
+        <CategoryFilter filter={handleFilterChange} cocktails={cocktails} />
+        <button onClick={resetFilter} type="button">Clear</button>
+      </div>
+      <div className="CocktailsList-list">
+        {
+          filteredCocktails.map((cocktail) => <CocktailCard cocktail={cocktail} key={cocktail} />)
         }
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 CocktailsList.propTypes = {
-  cocktails: PropTypes.objectOf.isRequired,
+  cocktails: PropTypes.objectOf,
+};
+
+CocktailsList.defaultProps = {
+  cocktails: {},
 };
 
 export default CocktailsList;
